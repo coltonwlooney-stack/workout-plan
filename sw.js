3893a0b3aac142ecbbf67174537afcc521d3a9f8
coltonwlooney-stack <coltonwlooney@gmail.com>
2026-04-20 13:50:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-plan-v1';
+const CACHE = 'workout-plan-v2';
 const ASSETS = ['/workout-plan/', '/workout-plan/index.html', '/workout-plan/app.js', '/workout-plan/style.css', '/workout-plan/manifest.json'];
 
 self.addEventListener('install', e => {
